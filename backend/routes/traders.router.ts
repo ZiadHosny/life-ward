87b1a7670a98309c29a699497295fe1ec982da89
AbdulrhmanMohamed <afarg843@gmail.com>
2024-08-08 09:16:47 +0200

    trader notification

--- backend/routes/traders.router.ts
+++ backend/routes/traders.router.ts
@@ -6,12 +6,14 @@ import {
   updateTrader,
   deleteTrader,
   verfiyTrader,
+  assignOrdersToTraders,
 } from "../controllers/trader.controller";
 
 const traderRouter = Router();
 
 traderRouter.post("/", createTrader);
 traderRouter.get("/", getTraders);
+traderRouter.post("/assignOrder", assignOrdersToTraders);
 traderRouter.post("/verify", verfiyTrader);
 traderRouter.get("/:id", getTraderById);
 traderRouter.put("/:id", updateTrader);
